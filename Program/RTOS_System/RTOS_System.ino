9219ce51786643a34c261e3e94fb7a23a8a3fc3e
João Paulo <jpcunha4@gmail.com>
2016-06-15 20:46:38 -0300

    The program seemsto be done, but I'll revise it later.

--- Program/RTOS_System/RTOS_System.ino
+++ Program/RTOS_System/RTOS_System.ino
@@ -1,25 +1,83 @@
 #include <NilRTOS.h>
 #include <NilSerial.h>
+#include <NilTimer1.h>
 
 #define Serial NilSerial
 
+const uint8_t yellowLed = 8;
+const uint8_t greenLed = 10;
+const uint8_t redLed = 12;
+
+const uint8_t semRedLed = 7;
+const uint8_t semGreenLed = 5; 
+
 //A semaphore with just one slot to be accessed.
-SEMAPHORE_DELC(sem, 1);
+SEMAPHORE_DECL(sem, 1);
+
+void CONTROL_SEM_LEDS(int signal){
+  if(signal == 1){
+    digitalWrite(semRedLed,LOW);
+    digitalWrite(semGreenLed,HIGH);
+  }else if(signal == 0){
+    digitalWrite(semRedLed,HIGH);
+    digitalWrite(semGreenLed,LOW);
+  }
+}
 
 void CRITICAL_REGION(int led, int computingTime){
-
+  //This is the place where the thrid running machine and fullfil the bottle
+  
+  CONTROL_SEM_LEDS(0);
+  
+  digitalWrite(led,HIGH);
+  Serial.println("Moving running machine");
+  nilThdSleepMilliseconds(computingTime);
+  digitalWrite(led,LOW);
+  Serial.println("Stoped running machine");
+  digitalWrite(redLed,HIGH);
+  Serial.println("Started fufilling the bottle");
+  nilThdSleepMilliseconds(5000);
+  digitalWrite(redLed,LOW);
+  Serial.println("Finished fulfilling the bottle\nMoved the third running machine");
+  
+  nilSemSignal(&sem);
+  CONTROL_SEM_LEDS(1);
 }
 
 NIL_WORKING_AREA(waRMachine1, 128);
 
 NIL_THREAD(RMachine1, arg){
   
+  uint32_t P = 1000,D = P, CT = 1000;
+  
+  while(true){
+  
+    msg_t test = nilSemWaitTimeout(&sem,P);
+    
+    if(test == NIL_MSG_OK){
+      CRITICAL_REGION(yellowLed,CT);
+      nilThdSleep(P-CT);
+    }
+  
+  }
+  
 }
 
 NIL_WORKING_AREA(waRMachine2, 128);
 
 NIL_THREAD(RMachine2, arg){
-
+  uint32_t P = 1000,D = P, CT = 1000;
+  
+  while(true){
+  
+    msg_t test = nilSemWaitTimeout(&sem,P);
+    
+    if(test == NIL_MSG_OK){
+      CRITICAL_REGION(greenLed,CT);
+      nilThdSleep(P-CT);
+    }
+  
+  }
 }
 
 NIL_THREADS_TABLE_BEGIN()
@@ -28,6 +86,12 @@ NIL_THREADS_TABLE_ENTRY("RMachine2",RMachine2,NULL,waRMachine2,sizeof(waRMachine
 NIL_THREADS_TABLE_END()
 
 void setup(){
+  
+  pinMode(yellowLed,OUTPUT);
+  pinMode(redLed,OUTPUT);
+  pinMode(greenLed,OUTPUT);
+  pinMode(semRedLed,OUTPUT);
+  pinMode(semGreenLed,OUTPUT);
   
   //Remember, never use as outputs doors 0 and 1
   Serial.begin(9600);
