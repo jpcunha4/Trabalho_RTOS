41152265c1013eaa1095ea47142fb05ea35d6e33
João Paulo <jpcunha4@gmail.com>
2016-06-15 00:03:56 -0300

    More Configuration

--- Program/RTOS_System/RTOS_System.ino
+++ Program/RTOS_System/RTOS_System.ino
@@ -3,10 +3,36 @@
 
 #define Serial NilSerial
 
+//A semaphore with just one slot to be accessed.
+SEMAPHORE_DELC(sem, 1);
+
+void CRITICAL_REGION(int led, int computingTime){
+
+}
+
+NIL_WORKING_AREA(waRMachine1, 128);
+
+NIL_THREAD(RMachine1, arg){
+  
+}
+
+NIL_WORKING_AREA(waRMachine2, 128);
+
+NIL_THREAD(RMachine2, arg){
+
+}
+
+NIL_THREADS_TABLE_BEGIN()
+NIL_THREADS_TABLE_ENTRY("RMachine1",RMachine1,NULL,waRMachine1,sizeof(waRMachine1))
+NIL_THREADS_TABLE_ENTRY("RMachine2",RMachine2,NULL,waRMachine2,sizeof(waRMachine2))
+NIL_THREADS_TABLE_END()
+
 void setup(){
   
   //Remember, never use as outputs doors 0 and 1
   Serial.begin(9600);
+  
+  nilSysBegin();
   
 }
 
